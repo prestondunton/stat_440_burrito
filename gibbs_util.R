3c30c729714060ce469d3431bb4512723726ea6a
Trevor Overton <toverton756@gmail.com>
2022-04-25 19:27:18 -0600

    Added truncated model and acf plots

--- gibbs_util.R
+++ gibbs_util.R
@@ -1,7 +1,11 @@
 library(mvtnorm)
+library(tmvtnorm)
 library(pracma)
 require(reshape2)
 library(knitr)
+library(forecast)
+library(patchwork)
+library(ggfortify)
 
 
 mlr_gibbs <- function(X_raw, y, mu, tau_2, a, b, iterations=10000) {
@@ -42,6 +46,41 @@ mlr_gibbs <- function(X_raw, y, mu, tau_2, a, b, iterations=10000) {
   return(posterior_dist)
 }
 
+truncated_gibbs <- function(X_raw, y, mu, tau_2, a, b, iterations=10000, lb=0, ub=Inf){
+  X <- cbind(1,X_raw)
+
+  tau_2_inv <- 1 / tau_2
+  n <- length(y)
+  p <- ncol(X)
+
+  posterior_dist <- matrix(NA, nrow=iterations, ncol=p+1)
+
+  beta <- drop(rtmvnorm(n=1, mean=mu, sigma=tau_2 * eye(p), lower=lb, upper=ub, algorithm="gibbs"))
+  sig_2_inv <- rgamma(1, a, b)
+
+  posterior_dist[1, ] <- c(beta, sig_2_inv)
+
+  XtX <- t(X) %*% X
+  Xty <- t(X) %*% y
+  I <- diag(p)
+
+  for ( i in seq(2, iterations)){
+    v <- solve(sig_2_inv * XtX + tau_2_inv * I)
+    m <- drop(v %*% (sig_2_inv * Xty + tau_2_inv * mu))
+    beta <- drop(rtmvnorm(n=1, mean=m, sigma=v, lower=lb, upper=ub, algorithm="gibbs"))
+
+    sig_2_inv <- rgamma(1, a+n/2, b+ 0.5 * t(y-X %*% beta) %*% (y- X %*% beta))
+
+    posterior_dist[i, ] <- c(beta, sig_2_inv)
+  }
+
+  sigma_column <- ncol(posterior_dist)
+  posterior_dist[, sigma_column] <- 1/sqrt(posterior_dist[,sigma_column])
+
+  colnames(posterior_dist) <- c('Intercept', colnames(X_raw), 'sigma')
+
+  return(posterior_dist)
+}
 
 plot_traces <- function(gibbs_distribution, title='', facets=TRUE) {
   
@@ -60,6 +99,15 @@ plot_traces <- function(gibbs_distribution, title='', facets=TRUE) {
   }
 }
 
+acf_plots <- function(dist){
+  plot_df <- melt(dist)
+  colnames(plot_df) <- c('Iteration','Parameter','Estimate')
+  acf.key<-list()
+  for(i in 1:length(unique(plot_df$Parameter))){
+    acf.key[[i]]<-acf(plot_df$Estimate[plot_df$Parameter==unique(plot_df$Parameter)[[i]]])
+  }
+  autoplot(acf.key, main="ACF Plots", nbin=5)
+}
 
 summarize_dist <- function(distribution, param_names, title='') {
   table = data.frame()
